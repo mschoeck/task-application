9c5a45a5c3a385462c7e0d9bc9b9601b34078dda
mschoeck <michael.schoeck@fiduciagad.de>
2017-11-11 02:15:24 +0100

    initial commit

---
+++ create-user.sql
@@ -0,0 +1,6 @@
+--CREATE USER TASKUSER
+--IDENTIFIED BY "taskuser"
+--DEFAULT TABLESPACE USERS
+--TEMPORARY TABLESPACE TEMP 
+
+--grant create session, create sequence, create table, create view, create trigger, create type, unlimited tablespace to TASKUSER
